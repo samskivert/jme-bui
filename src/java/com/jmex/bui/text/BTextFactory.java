99b691541d26e15e7a0524d617a97823d77014b6
Michael Bayne <mdb@threerings.net>
2007-12-16 01:52:30 +0000

    Nixed convenience methods as we no longer use them.

--- src/java/com/jmex/bui/text/BTextFactory.java
+++ src/java/com/jmex/bui/text/BTextFactory.java
@@ -25,8 +25,8 @@ import com.jme.renderer.ColorRGBA;
 import com.jmex.bui.BConstants;
 
 /**
- * Creates instances of {@link BText} using a particular technology and a
- * particular font configuration.
+ * Creates instances of {@link BText} using a particular technology and a particular font
+ * configuration.
  */
 public abstract class BTextFactory
     implements BConstants
@@ -37,8 +37,8 @@ public abstract class BTextFactory
     public abstract int getHeight ();
 
     /**
-     * Creates a text instance using our the font configuration associated
-     * with this text factory and the foreground color specified.
+     * Creates a text instance using our the font configuration associated with this text factory
+     * and the foreground color specified.
      */
     public BText createText (String text, ColorRGBA color)
     {
@@ -46,41 +46,18 @@ public abstract class BTextFactory
     }
 
     /**
-     * Creates a text instance using our the font configuration associated with
-     * this text factory and the foreground color, text effect and text effect
-     * color specified.
+     * Creates a text instance using our the font configuration associated with this text factory
+     * and the foreground color, text effect and text effect color specified.
      *
-     * @param useAdvance if true, the advance to the next insertion point will
-     * be included in the bounds of the created text (this is needed by
-     * editable text displays).
+     * @param useAdvance if true, the advance to the next insertion point will be included in the
+     * bounds of the created text (this is needed by editable text displays).
      */
-    public abstract BText createText (
-            String text, ColorRGBA color, int effect, int effectSize,
-            ColorRGBA effectColor, boolean useAdvance);
+    public abstract BText createText (String text, ColorRGBA color, int effect, int effectSize,
+                                      ColorRGBA effectColor, boolean useAdvance);
 
     /**
-     * Wraps a string into a set of text objects that do not exceed the
-     * specified width.
+     * Wraps a string into a set of text objects that do not exceed the specified width.
      */
-    public BText[] wrapText (String text, ColorRGBA color, int maxWidth)
-    {
-        return wrapText(text, color, NORMAL, DEFAULT_SIZE, null, maxWidth);
-    }
-
-    /**
-     * Wraps a string into a set of text objects that do not exceed the
-     * specified width.
-     */
-    public BText[] wrapText (String text, ColorRGBA color, int effect, int maxWidth)
-    {
-        return wrapText(text, color, effect, DEFAULT_SIZE, null, maxWidth);
-    }
-
-    /**
-     * Wraps a string into a set of text objects that do not exceed the
-     * specified width.
-     */
-    public abstract BText[] wrapText (
-        String text, ColorRGBA color, int effect, int effectSize,
-        ColorRGBA effectColor, int maxWidth);
+    public abstract BText[] wrapText (String text, ColorRGBA color, int effect, int effectSize,
+                                      ColorRGBA effectColor, int maxWidth);
 }
